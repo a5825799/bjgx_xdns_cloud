67b8d6dae47975299bc1f6ee1511dd09ec65cb02
a5825799 <18335196765@163.com>
2018-06-13 17:46:50 +0800

    spring_xdns_service_feign 服务器端信息

---
+++ spring_xdns_cloud_service_feign/src/main/java/com/xdns/cloud/controller/HiController.java
@@ -0,0 +1,21 @@
+package com.xdns.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xdns.cloud.service.SchedualServiceHi;
+
+@RestController
+public class HiController {
+	
+	@Autowired
+	SchedualServiceHi schedualServiceHi;
+	
+	@RequestMapping(value="/hi")
+	public String sayHi(@RequestParam String name){
+		return schedualServiceHi.sayHiFromClientOne(name);
+	}
+	
+}	
